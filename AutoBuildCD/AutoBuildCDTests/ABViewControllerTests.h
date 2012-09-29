857836366e5a3a2234c080c452d5b5beba782f5e
Victor Lima <victorloureirolima@gmail.com>
2012-09-29 16:17:40 -0300

    implementing first application tests for ABViewController

---
+++ AutoBuildCD/AutoBuildCDTests/ABViewControllerTests.h
@@ -0,0 +1,15 @@
+//
+//  ABViewControllerTests.h
+//  AutoBuildCD
+//
+//  Created by Victor Lima on 9/29/12.
+//  Copyright (c) 2012 concretesolutions. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+#import "ABViewController.h"
+
+@interface ABViewControllerTests : SenTestCase
+
+@end
---
+++ AutoBuildCD/AutoBuildCDTests/ABViewControllerTests.m
@@ -0,0 +1,19 @@
+//
+//  ABViewControllerTests.m
+//  AutoBuildCD
+//
+//  Created by Victor Lima on 9/29/12.
+//  Copyright (c) 2012 concretesolutions. All rights reserved.
+//
+
+#import "ABViewControllerTests.h"
+
+@implementation ABViewControllerTests
+
+-( void ) test_subviews_count
+{
+  ABViewController* abvc = [[ABViewController alloc] init];
+  STAssertTrue( [abvc.view.subviews count] == 1, @"" );
+}
+
+@end
